040ef842161cfc06af8f4b33b532424f59490bc2
MegAgainDev <c.meganjones@yahoo.com>
2022-02-15 12:31:57 -0500

    1 file modified, 2 added, 2 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,8 @@ export class AppComponent  {
   public Price;
   public Photo;
   public products = [
-    {Name:'Tv',Price:'15000',Photo:'https://stackblitz.com/files/angular-shopping-cart-nit/github/kjagannathreddy/angular-shopping-cart-nit/master/award.jpg'},
-    {Name:'Mobile',Price:'4000',Photo:'https://cdn.jsdelivr.net/gh/kjagannathreddy/angular-shopping-cart-nit@master/award.jpg'},
+    {Name:'iPhone',Price:'$1050.00',Photo:'.../images/iPhone.jpg'},
+    {Name:'Samsung TV',Price:'4000',Photo:'https://cdn.jsdelivr.net/gh/kjagannathreddy/angular-shopping-cart-nit@master/award.jpg'},
     {Name:'Fridge',Price:'14000',Photo:'https://cdn.jsdelivr.net/gh/kjagannathreddy/angular-shopping-cart-nit@master/award.jpg'},
     {Name:'Washing Machine',Price:'18000',Photo:'https://cdn.jsdelivr.net/gh/kjagannathreddy/angular-shopping-cart-nit@master/award.jpg'},
     {Name:'Sparx shoes',Price:'1000',Photo:'https://cdn.jsdelivr.net/gh/kjagannathreddy/angular-shopping-cart-nit@master/award.jpg'},
